51252f7a4543b7c3cb4cddf80c2c4f07fb86429a
MinSeokKun <kimms51567115@gmail.com>
2024-11-18 21:23:11 +0900

    회원가입 시 비밀번호 인코딩 / 로그인 시 JWT 토큰 발급

---
+++ AInServer/ain/src/main/java/com/team/ain/dto/MemberProfile.java
@@ -0,0 +1,14 @@
+package com.team.ain.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberProfile {
+
+    private int id;
+
+    private String profilePictureUrl;
+
+}
--- AInServer/ain/src/main/java/com/team/ain/mapper/PetMapper.java
+++ AInServer/ain/src/main/java/com/team/ain/mapper/PetMapper.java
@@ -5,12 +5,13 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.team.ain.dto.Pet;
+import com.team.ain.dto.PetRegist;
 
 @Mapper
 public interface PetMapper {
 
 
-    void insertPet(Pet pet);
+    void insertPet(PetRegist pet);
 
     Pet getPetById(int id);
 
