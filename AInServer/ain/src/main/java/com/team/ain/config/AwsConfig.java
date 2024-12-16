f14d760483aa8656eac57d57975c85be63ef6937
MinSeokKun <kimms51567115@gmail.com>
2024-12-16 18:29:55 +0900

    aws 및 post

--- AInServer/ain/src/main/java/com/team/ain/config/AwsConfig.java
+++ AInServer/ain/src/main/java/com/team/ain/config/AwsConfig.java
@@ -1,35 +1,35 @@
 package com.team.ain.config;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+import io.github.cdimascio.dotenv.Dotenv;
 
 @Configuration
 public class AwsConfig {
 
     @Bean
     public AmazonS3 amazonS3() {
-        // Dotenv 사용하여 .env 파일 로드
         Dotenv dotenv = Dotenv.configure().load();
 
-        // .env 파일에서 환경 변수 읽기
-        String accessKey = dotenv.get("AWS_ACCESS_KEY");
+        String accessKeyId = dotenv.get("AWS_ACCESS_KEY_ID");
         String secretKey = dotenv.get("AWS_SECRET_ACCESS_KEY");
         String region = dotenv.get("AWS_REGION");
 
-        if (accessKey == null || secretKey == null || region == null) {
+        if (accessKeyId == null || secretKey == null || region == null) {
             throw new IllegalArgumentException("AWS credentials cannot be null.");
         }
 
-        // AWS S3 클라이언트 생성
-        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKeyId, secretKey);
 
         return AmazonS3ClientBuilder.standard()
-                .withRegion(region)
+                .withRegion(Regions.fromName(region))
                 .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
                 .build();
     }
