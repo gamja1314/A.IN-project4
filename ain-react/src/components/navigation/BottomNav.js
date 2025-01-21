8faba94b1dd67dc44c7c91aec49b57ce88432832
MinSeokKun <kimms51567115@gmail.com>
2025-01-21 14:33:15 +0900

    Update BottomNav.js

--- ain-react/src/components/navigation/BottomNav.js
+++ ain-react/src/components/navigation/BottomNav.js
@@ -24,6 +24,12 @@ export const BottomNav = ({ currentPage, onPageChange, refreshTrigger = 0 }) => 
   return (
     <nav className="fixed bottom-0 left-0 right-0 h-16 bg-white border-t flex items-center justify-around z-[1000] max-w-md mx-auto">
       <button 
+        className={`flex flex-col items-center justify-center w-1/5 ${currentPage === 'home' ? 'text-blue-500' : ''}`}
+        onClick={() => onPageChange('home')}
+      >
+        <span className="text-sm">홈</span>
+      </button>
+      <button 
         className={`flex flex-col items-center justify-center w-1/5 ${currentPage === 'place' ? 'text-blue-500' : ''}`}
         onClick={() => onPageChange('place')}
       >
@@ -41,18 +47,6 @@ export const BottomNav = ({ currentPage, onPageChange, refreshTrigger = 0 }) => 
             </span>
           )}
         </div>
-      </button>
-      <button 
-        className={`flex flex-col items-center justify-center w-1/5 ${currentPage === 'home' ? 'text-blue-500' : ''}`}
-        onClick={() => onPageChange('home')}
-      >
-        <span className="text-sm">홈</span>
-      </button>
-      <button 
-        className={`flex flex-col items-center justify-center w-1/5 ${currentPage === 'search' ? 'text-blue-500' : ''}`}
-        onClick={() => onPageChange('search')}
-      >
-        <span className="text-sm">검색</span>
       </button>
       <button 
         className={`flex flex-col items-center justify-center w-1/5 ${currentPage === 'mypage' ? 'text-blue-500' : ''}`}
