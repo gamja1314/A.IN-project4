28660bd9087bbd75ccf930161133f049336d4aa6
gamja1314 <gamja941104@gmail.com>
2025-04-21 16:21:36 +0900

    구글 소셜 로그인 확인

--- AInServer/ain/src/main/java/com/team/ain/config/oauth2/OAuth2UserInfoFactory.java
+++ AInServer/ain/src/main/java/com/team/ain/config/oauth2/OAuth2UserInfoFactory.java
@@ -4,11 +4,13 @@ import java.util.Map;
 
 public class OAuth2UserInfoFactory {
 
-        public static OAuth2UserInfo getOAuth2UserInfo(String registrationId, Map<String, Object> attributes) {
+    public static OAuth2UserInfo getOAuth2UserInfo(String registrationId, Map<String, Object> attributes) {
         if(registrationId.equalsIgnoreCase("kakao")) {
             return new KakaoOAuth2UserInfo(attributes);
         } else if(registrationId.equalsIgnoreCase("naver")) {
             return new NaverOAuth2UserInfo(attributes);
+        } else if(registrationId.equalsIgnoreCase("google")) {
+            return new GoogleOAuth2UserInfo(attributes);
         } else {
             throw new IllegalArgumentException("지원하지 않는 소셜 로그인입니다.");
         }
